e8ce9cf50de46caa3d98583c8e70fe7db35c91f6
Abdullah Al Akash <aabdullahalakash@gmail.com>
2023-10-07 00:22:24 +0600

    Update Method Implement

--- index.js
+++ index.js
@@ -37,14 +37,39 @@ async function run() {
             console.log(user);
             const result = await userCollection.insertOne(user);
             res.send(result);
-        })
+        });
 
         // Get All User:
         app.get('/users', async (req, res) => {
             const cursor = userCollection.find();
             const result = await cursor.toArray();
             res.send(result);
+        });
+
+        // Update Single User:
+        app.put('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const user = req.body;
+            const filter = { _id: new ObjectId(id) };
+            const options = { upsert: true };
+            const updatedUser = {
+                $set: {
+                    name: user.name,
+                    email: user.email,
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedUser, options);
+            res.send(result);
         })
+
+        // Get Single User API:
+        app.get('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const user = await userCollection.findOne(query);
+            res.send(user)
+        });
+
 
         // Delete Single User:
         app.delete('/users/:id', async (req, res) => {
@@ -52,7 +77,7 @@ async function run() {
             const query = { _id: new ObjectId(id) };
             const result = await userCollection.deleteOne(query);
             res.send(result);
-        })
+        });
 
 
         // Send a ping to confirm a successful connection
